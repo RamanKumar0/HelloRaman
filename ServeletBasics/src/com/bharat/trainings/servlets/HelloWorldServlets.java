bb966e44f5ba50920b2451b826da83733c7f6a4c
raman <raman@LAPTOP-MDVCAG5V>
2020-08-21 08:15:46 +0530

    @commiting a project

---
+++ ServeletBasics/src/com/bharat/trainings/servlets/HelloWorldServlets.java
@@ -0,0 +1,27 @@
+package com.bharat.trainings.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class HelloWorldServlets extends GenericServlet {
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Hello Servelts World</h1>");
+		out.println("</body>");
+		out.println("</html>");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
